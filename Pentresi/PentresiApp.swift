6bdcb684400f357691f52da16f26215de05c4e5f
Pablo Salas <pablosalas_@outlook.com>
2024-03-05 17:07:28 -0600

    Initial Commit

---
+++ Pentresi/PentresiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PentresiApp.swift
+//  Pentresi
+//
+//  Created by Pablo Salas on 05/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PentresiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
